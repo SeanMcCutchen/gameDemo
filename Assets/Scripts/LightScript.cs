76a1e1620e4b457b8543a62d19fb005c3bad22d9
jsiegal94 <jsiegal@CS-XENA19.trinity.local>
2016-02-06 15:00:33 -0600

    First Commit

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightScript : MonoBehaviour {
+	GameObject myLight;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/*
+	public void lightToggle() {
+		myLight = GameObject.Find ("myLight").GetComponent (Light);
+	}
+*/
+
+}
---
+++ Assets/Scripts/babyScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class babyScript : MonoBehaviour {
+	GameObject baby;
+	AudioSource sound;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void babyisAwake() {
+		baby = GameObject.Find ("baby");
+		Debug.Log ("You win");
+
+	}
+
+}
